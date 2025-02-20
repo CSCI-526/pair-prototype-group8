82a263b9790ecd8a7398e9e27c55e6d369fffd5f
chessyBoom <kwrrwytinii24@gmail.com>
2025-02-19 19:03:08 -0800

    feat: implement stars for levels

---
+++ Assets/Scripts/LevelSelectionManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionManager : MonoBehaviour
+{
+    public static string type = "NEW Level ";
+    public static int currentLevel;
+    // Start is called before the first frame update
+
+}
--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -7,19 +7,37 @@ using TMPro;
 
 public class LevelSelector : MonoBehaviour
 {
-
     public int level;
     public TextMeshProUGUI levelText;
+
+    public GameObject zeroStar;
+    public GameObject oneStar;
+    public GameObject twoStar;
+    public GameObject threeStar;
+
+    private GameObject currentStar;
 
     // Start is called before the first frame update
     void Start()
     {
         levelText.text = level.ToString();
+        int starCount = PlayerPrefs.GetInt("stars" + LevelSelectionManager.type + level.ToString(), 0);
+        Debug.Log("star count: " + starCount);
+        SpawnStar(starCount);
+    }
+
+    private void SpawnStar(int starCount)
+    {
+        zeroStar.SetActive(starCount == 0);
+        oneStar.SetActive(starCount == 1);
+        twoStar.SetActive(starCount == 2);
+        threeStar.SetActive(starCount == 3);
     }
 
     public void OpenLevelScene() {
-        var type = ToggleSwitch.CurrentValue ? "WackyLevel" : "NEW Level ";
-        Debug.Log("Current type: " + type);
-        SceneManager.LoadScene(type + level.ToString());
+        // var type = ToggleSwitch.CurrentValue ? "WackyLevel" : "NEW Level ";
+        LevelSelectionManager.currentLevel = level;
+        Debug.Log("current select: " + LevelSelectionManager.currentLevel);
+        SceneManager.LoadScene(LevelSelectionManager.type + level.ToString());
     }
 }
